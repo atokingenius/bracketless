ac81484d1cc21af8075e0931b536b375bb607196
Chris Barrozo <atokingenius@gmail.com>
2025-01-05 15:10:33 -0800

    Removing unused attribute

--- src/cssStylesheet.ts
+++ src/cssStylesheet.ts
@@ -1,7 +1,6 @@
 import {cssClass} from "./cssClass.ts";
 export class cssStylesheet {
 	#classes: cssClass[] = [];
-	filename: string;
 	addClass(name: string): cssClass {
 		const newClass = new cssClass(name);
 		this.#classes.push(newClass);
@@ -10,7 +9,7 @@ export class cssStylesheet {
 	toString(): string {
 		return this.#classes.map((cssClass): string => {return cssClass.toString()}).join('\n');
 	}
-	constructor(filename: string) {
-		this.filename = filename;
+	constructor() {
+
 	}
 }
